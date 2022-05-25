a4594d3b0e1596412f21392c20775ef9c517fb28
tahsinkarim <tahsinnkarim@gmail.com>
2022-05-25 19:25:25 +0600

    updated images

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ menuBtn.addEventListener('click', ()=>{
     menuBtn.classList.add('active');
     sidebar.classList.add('active');
     overlay.classList.add('active');
-    hamburger.src = "/images/icon-close-menu.svg"
+    hamburger.src = "./images/icon-close-menu.svg"
     menuOpen=true;
   } else{
     menuBtn.classList.remove('active');
     sidebar.classList.remove('active');
     overlay.classList.remove('active');
-    hamburger.src = "/images/icon-menu.svg"
+    hamburger.src = "./images/icon-menu.svg"
     menuOpen=false;
   };
 
@@ -33,22 +33,22 @@ let dropOpen = false;
 featuresBtn.addEventListener('click', ()=>{
   if(!dropOpen){
     features.classList.add('active');
-    featuresArrow.src = "/images/icon-arrow-up.svg"
+    featuresArrow.src = "./images/icon-arrow-up.svg"
     dropOpen= true;
   } else {
     features.classList.remove('active');
-    featuresArrow.src = "/images/icon-arrow-down.svg"
+    featuresArrow.src = "./images/icon-arrow-down.svg"
     dropOpen= false;
   }
 })
 companyBtn.addEventListener('click', ()=>{
   if(!dropOpen){
     company.classList.add('active');
-    companyArrow.src = "/images/icon-arrow-up.svg"
+    companyArrow.src = "./images/icon-arrow-up.svg"
     dropOpen= true;
   } else {
     company.classList.remove('active');
-    companyArrow.src = "/images/icon-arrow-down.svg"
+    companyArrow.src = "./images/icon-arrow-down.svg"
     dropOpen= false;
   }
 })
